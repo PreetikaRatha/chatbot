1d534288e828c5a85e80a47dda69598abfe7e5f4
PreetikaRatha <preetikaratha09@gmail.com>
2026-07-16 00:03:48 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,12 @@
-from google import genai
 import streamlit as st
+from google import genai
 
 client = genai.Client(api_key=st.secrets["API_KEY"])
 
-st.title("🤖 Gemini Chatbot")
+st.write("Available models:")
 
-prompt = st.chat_input("Type your message...")
-
-if prompt:
-    st.chat_message("user").write(prompt)
-
-    response = client.models.generate_content(
-        model="gemini-2.5-pro",
-        contents=prompt,
-    )
-
-    st.chat_message("assistant").write(response.text)
+try:
+    for model in client.models.list():
+        st.write(model.name)
+except Exception as e:
+    st.error(e)
